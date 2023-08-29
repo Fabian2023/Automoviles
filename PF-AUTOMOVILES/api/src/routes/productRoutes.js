93c563762335ec95ac9dbed39dd581c797caa156
Chirstian Camilo Barrera Salinas <camilobr89@hotmail.com>
2023-08-29 17:16:12 -0500

    se crear modelos y controller de productos

---
+++ PF-AUTOMOVILES/api/src/routes/productRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const ProductController = require('../controllers/productController'); 
+
+const router = express.Router();
+
+router.post('/product', ProductController.create);
+router.put('/product/:id', ProductController.update);
+// router.delete('/product/:id', ProductController.delete);
+router.get('/product', ProductController.getAll);
+// router.get('/product/:id', ProductController.getById);
+
+module.exports = router;
